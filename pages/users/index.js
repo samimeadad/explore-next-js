ee73d2dfcd9c045f83bf2e0e736cf5dfc273e904
samimeadad <sami.meadad@gmail.com>
2021-11-21 19:56:36 +0600

    overview of next.js done

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 
 const index = ( { users } ) => {
     return (
         <div>
             <h2>This is Users Page. Total Users: { users.length }</h2>
             {
-                users.map( user => <p key={ user.id }>{ user.name }</p> )
+                users.map( user => <p key={ user.id }>
+                    { user.name } <button><Link href={ `users/${ user.id }` }><a>Explore the User</a></Link></button>
+                </p> )
             }
         </div >
     );
